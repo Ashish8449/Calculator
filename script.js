765ce4a21f68e84d800e4e758847f46bc74fae3b
ashish8449 <ashishgururani8449@gmail.com>
2021-11-05 01:03:28 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById("input");
+let buttonText;
+let buttons = document.querySelectorAll("button");
+let value = "";
+for (item of buttons) {
+  console.log(item);
+  item.addEventListener("click", (e) => {
+    //   console.log(item);
+    buttonText = e.target.innerText;
+    if (buttonText == "x") {
+      buttonText = "*";
+      screen.value += buttonText;
+      value += buttonText;
+    } else if (buttonText == "=") {
+      screen.value = eval(value);
+    } else if (buttonText == "C") {
+      screen.value = "";
+      value = "";
+    } else if (buttonText == "del") {
+      value = value.slice(0, value.length - 1);
+      screen.value = screen.value.slice(0, screen.value.length - 1);
+    } else {
+      screen.value += buttonText;
+      value += buttonText;
+    }
+    console.log(value);
+  });
+}
